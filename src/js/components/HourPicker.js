5a397274a74f9374417035fd13565f4fc8c35a35
Marta Semrau <mm.semrau@gmail.com>
2020-08-14 12:03:18 +0200

    10.3 correct code

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,43 +1,45 @@
-import {BaseWidget} from './BaseWidget.js'
-import {select, settings } from '../settings.js'
-import {utils} from '../utils.js'
+/* global rangeSlider */
+ 
+import BaseWidget  from './BaseWidget.js';
+import { select, settings } from '../settings.js';
+import { utils } from '../utils.js';
 
 class HourPicker extends BaseWidget {
-    constructor (wrapper){
-        super(wrapper, settings.hours.open);
-        const thisWidget = this;
+  constructor(wrapper) {
+    super(wrapper, settings.hours.open);
+    const thisWidget = this;
 
-        thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
-        thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.output);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.output);
 
-        thisWidget.initPlugin();
-        thisWidget.value = thisWidget.dom.input.value;
+    thisWidget.initPlugin();
+    thisWidget.value = thisWidget.dom.input.value;
 
-    }
-        
-        initPlugin() {
-            const thisWidget = this;
-            rangeSlider.create(thisWidget.dom.input);
+  }
 
-            thisWidget.dom.input.addEventListener('input', function (){
-                thisWidget.value = thisWidget.dom.input.value;
-            })
+  initPlugin() {
+    const thisWidget = this;
+    rangeSlider.create(thisWidget.dom.input);
 
-        }
+    thisWidget.dom.input.addEventListener('input', function () {
+      thisWidget.value = thisWidget.dom.input.value;
+    });
 
-        parseValue(value) {
-            return utils.numberToHour(value);
-        }
+  }
 
-        isValid(){
-            return true;
-        }
-        renderValue() {
-            const thisWidget = this;
+  parseValue(value) {
+    return utils.numberToHour(value);
+  }
 
-            thisWidget.dom.output.innerHTML = thisWidget.value;
-        }
+  isValid() {
+    return true;
+  }
+  renderValue() {
+    const thisWidget = this;
 
+    thisWidget.dom.output.innerHTML = thisWidget.value;
+  }
 
-    
 }
+
+export default HourPicker;
